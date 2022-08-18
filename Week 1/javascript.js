e4cf02408f1ba0ded72654d6aa2d7259d2c81076
RileyHDeakin <109202681+RileyHDeakin@users.noreply.github.com>
2022-08-18 16:09:56 +1000

    Add files via upload

---
+++ Week 1/javascript.js
@@ -0,0 +1,25 @@
+function display_success_msg() {
+	document.getElementById("message").innerHTML = "this is a message brought to you by javascript";
+}
+
+
+function MouseDown() {
+   document.getElementById("paragraph_1").style.color = "pink";
+}
+
+function Mouseup() {
+   document.getElementById("paragraph_1").style.color = "black";
+   }
+   
+   
+function popup() {
+   let text;
+ let person = prompt("Please enter your name:");
+ if (person == null || person == "") {
+   text = "cancelled";
+ } else {
+   text = "Hello " + person;
+ }
+ document.getElementById("text_stuff").innerHTML = text;
+}
+
